b21f6efcf33a4c790300aabfce4e0a3a619b7fde
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-11-16 22:07:44 +0600

    Added component AddFilmForm

---
+++ src/components/AddFilmForm.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+interface Props extends React.PropsWithChildren {
+  onAdd: React.MouseEventHandler;
+  onFilmChange?: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+const AddFilmForm: React.FC<Props> = ({
+                                        onAdd,
+                                        onFilmChange
+                                      }) => {
+  return (
+    <div className='row m-1 g-3'>
+      <div className='col-auto'>
+        <input type='text'
+               placeholder="Add new film"
+               className="form-control"
+               onChange={onFilmChange}
+        />
+      </div>
+      <div className='col-auto'>
+        <button className="btn btn-primary"
+                onClick={onAdd}>Add film</button>
+      </div>
+    </div>
+  );
+};
+
+export  default AddFilmForm;
