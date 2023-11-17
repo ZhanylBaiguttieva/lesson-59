a9d76d6c7fe68cc2e8526eefaa0a93452f61aee5
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-11-17 16:54:37 +0600

    Added component Joke in App.tsx

---
+++ src/components/Joke.tsx
@@ -0,0 +1,30 @@
+import {useState} from 'react';
+
+interface JokeType {
+  value: string;
+}
+const url = 'https://api.chucknorris.io/jokes/random';
+const Joke = () => {
+  const [jokes, setJokes] = useState<JokeType>();
+
+    const fetchData = async () => {
+      const response = await fetch(url);
+      if (response.ok) {
+        const jokes = await response.json();
+        const newValue = jokes.value;
+        setJokes( {value: newValue});
+        console.log(newValue);
+      }
+    };
+
+  return (
+    <div>
+      <div className="bg-warning p-3 m-2 rounded">
+        <p>{jokes?.value}</p>
+      </div>
+      <button className="btn btn-success mb-3" onClick={fetchData}>Get joke</button>
+    </div>
+  );
+};
+
+export default Joke;
