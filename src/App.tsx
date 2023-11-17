cadc110b91dfcd0370f899c1b4c300d7a7891695
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-11-17 16:54:19 +0600

    Added component Film in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,83 @@
+import React, {useEffect, useState} from 'react';
+import Film from './components/Film';
+import AddFilmForm from './components/AddFilmForm';
 import './App.css';
+import Joke from './components/Joke';
+
+interface ExampleOfFilm {
+  info: string;
+  id: number;
+}
 
 function App() {
+  const [films, setFilms] = useState <ExampleOfFilm[]> ([
+    {info: 'GodFather', id: 1},
+    {info: 'Pretty little liers', id: 2},
+    {info: 'Batman', id: 3},
+  ]);
+  const [currentFilm, setCurrentFilm] = useState('');
+
+  useEffect(() => {
+    console.log('Films changed');
+  }), [films];
+  const changeFilm = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentFilm(event.target.value);
+  };
+
+  const changeFilmName = (event: React.ChangeEvent<HTMLInputElement>, id: number) => {
+    const newName = event.target.value;
+
+    setFilms((prevState) => prevState.map((film) => {
+      if (film.id === id) {
+        return {...film, info: newName};
+      }
+
+      return film;
+    }));
+  };
+
+  const deleteFilm = (id:number) => {
+    setFilms((prevState ) => prevState.filter(film => {
+      return film.id !== id;
+    }));
+  };
+
+  const addFilm = () => {
+    setFilms(prevState => {
+      const lastItem = prevState[prevState.length - 1];
+      const newId = lastItem ? lastItem.id + 1 : 1;
+      return [...prevState,{
+        id: newId,
+        info: currentFilm,
+      }];
+    });
+  };
 
   return (
     <>
-
+      <div className='mt-2 border mb-5 pb-4'>
+        <h2 className="fs-3 mt-5">Task 1</h2>
+        <AddFilmForm
+          onFilmChange = {(event) => changeFilm(event)}
+          onAdd = {() => addFilm()}
+        >
+        </AddFilmForm>
+        {films.length !== 0 &&
+          films.map(film =>
+            <Film
+              key={film.id}
+              info={film.info}
+              onFilmNameChange={(event) => changeFilmName(event,film.id)}
+              onDelete={() => deleteFilm(film.id)}
+            >
+            </Film>)}
+      </div>
+      <div className="border">
+        <h2 className="fs-3 mt-5">Task-2</h2>
+        <Joke/>
+      </div>
     </>
   );
 }
-
 export default App;
+
--- src/components/Film.tsx
+++ src/components/Film.tsx
@@ -1,33 +1,42 @@
-import React from "react";
+import React, {useEffect} from 'react';
 interface Props extends React.PropsWithChildren {
   info: string;
   onDelete: React.MouseEventHandler;
   onFilmNameChange?: React.ChangeEventHandler<HTMLInputElement>;
 }
-const Film: React.FC<Props> = ({
+const Film: React.FC<Props> = React.memo(({
                                  info,
                                  onDelete,
                                  onFilmNameChange,
                                  children
                                }) => {
+
+  console.log('[Film] render');
+
+  useEffect(() => {
+    console.log('[Film] mounted/updated');
+  });
   return (
     <div className='card m-2'>
       <div className='row'>
         <div className='col'>
           <input
-                 className="fs-3"
-                 value={info}
-                 onChange={onFilmNameChange}
-            />
+            className="fs-5 fw-normal"
+            value={info}
+            onChange={onFilmNameChange}
+          />
         </div>
         <div className='col'>
           <button className='btn btn-secondary'
-                  onClick={onDelete}>Delete</button>
+                  onClick={onDelete}>Delete
+          </button>
         </div>
       </div>
       {children}
     </div>
   );
-};
+}, (prevProps, nextProps) => {
+    return prevProps.info === nextProps.info;
+});
 
 export  default Film;
